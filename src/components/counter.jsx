036af99c37bf9d14d17a1685a2f5a4fd02bc8f51
Abhishek Singh <abhiwanttochat@gmail.com>
2019-06-12 00:25:11 +0530

    formatCount function added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <React.Fragment>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
