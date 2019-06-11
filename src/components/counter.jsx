b4edfe0f626b37bd728b8d75cac805b4d63c640c
Abhishek Singh <abhiwanttochat@gmail.com>
2019-06-12 00:36:57 +0530

    className attribute added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,13 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imageUrl: "https://picsum.photos/200"
+    count: 0
   };
   render() {
     return (
       <React.Fragment>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary  btn-sm">Increment</button>
       </React.Fragment>
     );
   }
