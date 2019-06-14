abaed62493d4051d7f45754738374081c08aed84
Abhishek Singh <abhiwanttochat@gmail.com>
2019-06-14 07:47:07 +0530

    Composing Components

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,7 +4,7 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
+      { id: 1, value: 3 },
       { id: 1, value: 0 },
       { id: 1, value: 0 },
       { id: 1, value: 0 }
@@ -14,7 +14,7 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} />
+          <Counter key={counter.id} value={counter.value} />
         ))}
       </div>
     );
