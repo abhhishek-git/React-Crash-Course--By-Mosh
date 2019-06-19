521906872ad27f8282369fb8a3c9c91562e0dafa
Abhishek Singh <abhiwanttochat@gmail.com>
2019-06-19 07:47:39 +0530

    Delete Button added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,16 +10,20 @@ class Counter extends Component {
   };
 
   render() {
-    console.log(this.props);
     return (
       <div>
-        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
           className="btn btn-secondary  btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,13 +10,21 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+
+  handleDelete = () => {
+    console.log("Event Handler called");
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value}>
-            <h4>Counter #{counter.id}</h4>
-          </Counter>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+            id={counter.id}
+          />
         ))}
       </div>
     );
